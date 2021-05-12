07e21d857283f24ba3e7b2d8246dafd776c3c745
Elena <ivankina.es@gmail.com>
2021-05-12 19:33:11 +0300

    new parameters for deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import { Container, Row, Col } from "react-bootstrap";
 
