9b7a808ff6e1ee9f428fc5491169c335adefa3d5
Elena <ivankina.es@gmail.com>
2021-05-20 15:50:44 +0300

    added spinner on the recipes page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import About from "../pages/About";
-import Home from "../pages/Home";
 import Recipes from "../pages/Recipes";
 import FormRecipe from "../pages/FormRecipe";
+
+import Spinner from "react-bootstrap/Spinner";
 
 import axios from "axios";
 
 const Main = () => {
 	const [recipes, setRecipes] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
-		axios
-			.get("https://secret-shore-65901.herokuapp.com/recipes/all")
-			.then((res) => setRecipes(res.data));
-	}, []);
+		axios.get("https://secret-shore-65901.herokuapp.com/recipes/all").then((res) => {
+			setRecipes(res.data);
+			setLoading(true);
+		});
+	});
 
 	return (
 		<main>
@@ -27,9 +30,13 @@ const Main = () => {
 				</Route>
 				<Route exact path="/">
 					<Recipes recipes={recipes} />
+					{loading === false && <h1>Page is loading...</h1>}
 				</Route>
 				<Route path="/recipes">
 					<Recipes recipes={recipes} />
+					{loading === false && (
+						<Spinner className="recipeSpinner" animation="border" variant="secondary" />
+					)}
 				</Route>
 			</Switch>
 		</main>
